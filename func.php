8ad2c764a5120508cbf17d89a0ab19e4c6c242fa
shakilmahammud <hshakil987@gmail.com>
2020-09-28 00:20:16 +0600

    php

---
+++ func.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($num1,$num2){
+    echo $num1+$num2;
+    echo '<br>';
+
+}
+add(10,20);
+function sub($num1,$num2){
+    echo $num1-$num2;
+    echo '<br>';
+
+}
+sub(10,20);
+function multi($num1,$num2){
+    echo $num1*$num2;
+    echo '<br>';
+
+}
+multi(10,20);
+function division($num1,$num2){
+    echo $num1/$num2;
+    echo '<br>';
+
+}
+division(10,20);
+?>
--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 $shakil='hello 5minutes';
 echo $shakil;
+$num1=20;
+$num2=25;
+$num3=5;
+$resultAdd=$num1+$num2;
+$resultSub=$num1-$num2;
+$resultMultiplication=$num1*$num2;
+$resultDivision=$num1/$num2;
+echo  '<br>','Add=',$resultAdd.'<br>',' Sub=','',$resultSub.'<br>','Multipli= ',' ',$resultMultiplication.'<br>',' Division=',' ',$resultDivision.'<br>';
+$vasesh=$num1%$num3;
+echo 'Moduls=',' ', $vasesh;
 ?>
